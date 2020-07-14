f721f57abaaec883f8f620db9011068ece5e8e8e
kateMoscoso <katiemscs@gmail.com>
2020-07-14 19:58:40 +0200

    Iterator pattern

---
+++ src/main/java/com/iterator/IteratorEverydayDemo.java
@@ -0,0 +1,23 @@
+package main.java.com.iterator;
+
+import java.util.*;
+
+public class IteratorEverydayDemo {
+    public static void main (String[] args){
+        List <String> names = new ArrayList<>();
+        //Set<String> names = new HashSet<>();
+
+        names.add("Bryan");
+        names.add("Aaron");
+        names.add("Jason");
+
+        Iterator<String> namesItr = names.iterator();
+
+        while(namesItr.hasNext()) {
+            String name = namesItr.next();
+            System.out.println(name);
+            namesItr.remove();
+        }
+        System.out.println("Names size" + names.size());
+    }
+}
