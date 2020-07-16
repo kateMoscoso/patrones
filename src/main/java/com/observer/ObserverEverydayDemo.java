7ca1f78df7c03e548a70a81d27cc8b7655927138
kateMoscoso <katiemscs@gmail.com>
2020-07-16 15:00:34 +0200

    Oberver pattern

---
+++ src/main/java/com/observer/ObserverEverydayDemo.java
@@ -0,0 +1,41 @@
+package main.java.com.observer;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.Flow;
+
+public class ObserverEverydayDemo {
+    public static void main (String[] args) {
+        TwitterStream messageStream = new TwitterStream();
+        Client client1 = new Client("John");
+        Client client2 = new Client("Bran");
+
+        messageStream.addObserver(client1);
+        messageStream.addObserver(client2);
+
+        messageStream.someoneTweeted();
+
+    }
+}
+
+// concrete subject
+class TwitterStream extends Observable {
+    public void someoneTweeted(){
+        setChanged();
+        notifyObservers();
+    }
+}
+
+//concrete observe
+class Client implements Observer {
+    private String name;
+
+    Client(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("Update " + name + "'s stream tweeted something.");
+    }
+}
