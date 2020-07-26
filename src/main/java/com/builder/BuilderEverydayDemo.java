fe660828087733c3387a4b0b050d95ee5afd30be
kateMoscoso <katiemscs@gmail.com>
2020-07-26 17:25:08 +0200

    builder pattern: examples

--- src/main/java/com/builder/BuilderEverydayDemo.java
+++ src/main/java/com/builder/BuilderEverydayDemo.java
@@ -1,4 +1,10 @@
 package main.java.com.builder;
 
 public class BuilderEverydayDemo {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("This is an example");
+        builder.append("of bla blha");
+    }
 }
--- src/main/java/com/builder/LunchOrderDemo.java
+++ src/main/java/com/builder/LunchOrderDemo.java
@@ -1,4 +1,16 @@
 package main.java.com.builder;
 
 public class LunchOrderDemo {
+    public static void main(String[] args) {
+    LunchOrder.Builder builder = new LunchOrder.Builder();
+
+    builder.bread("Meat").condiments("Lettuce").dressing("Mayo").meat("Turkey");
+
+    LunchOrder lunchOrder = builder.build();
+
+    System.out.println(lunchOrder.getBread());
+    System.out.println(lunchOrder.getCondiments());
+    System.out.println(lunchOrder.getDressing());
+
+    }
 }
